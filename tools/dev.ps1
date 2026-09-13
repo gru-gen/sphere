ce36445ffe92947bcac63a626ca30ac7e8136151
gru-gen <gruzjeka@gmail.com>
2026-09-13 10:59:37 +0300

    (kafka ): add kafka the stack - kraft single node

--- tools/dev.ps1
+++ tools/dev.ps1
@@ -23,3 +23,21 @@ function Add-BasketDb {
 function Stop-SphereDb {
     docker rm -f sphere-db
 }
+
+function Start-SphereKafka {
+    docker run -d --name sphere-kafka `
+        -p 9094:9094 `
+        -e KAFKA_NODE_ID=1 `
+        -e KAFKA_PROCESS_ROLES=broker,controller `
+        -e KAFKA_CONTROLLER_QUORUM_VOTERS=1@localhost:9093 `
+        -e KAFKA_LISTENERS=INTERNAL://:9092,CONTROLLER://:9093,HOST://:9094 `
+        -e KAFKA_ADVERTISED_LISTENERS=INTERNAL://localhost:9092,HOST://localhost:9094 `
+        -e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT,HOST:PLAINTEXT `
+        -e KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER `
+        -e KAFKA_INTER_BROKER_LISTENER_NAME=INTERNAL `
+        -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 `
+        -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1 `
+        -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 `
+        -e KAFKA_AUTO_CREATE_TOPICS_ENABLE=false `
+        apache/kafka:4.1.0
+}
